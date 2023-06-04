169054bab48308cb3ae566f2da6dacb95a9abaf3
kdb621 <liradb2000@live.co.kr>
2023-06-05 02:07:49 +0900

    update docu config;

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -6,20 +6,22 @@ const darkCodeTheme = require("prism-react-renderer/themes/dracula");
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-  title: "My Site",
-  tagline: "Dinosaurs are cool",
+  title: "키오스크 매뉴얼",
+  tagline: "manual-kiosk",
   favicon: "img/favicon.ico",
 
   // Set the production url of your site here
-  url: "https://your-docusaurus-test-site.com",
+  url: "https://liradb2000.github.io",
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: "/",
+  baseUrl: "/ecole-doc",
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: "facebook", // Usually your GitHub org/user name.
-  projectName: "docusaurus", // Usually your repo name.
+  organizationName: "liradb2000", // Usually your GitHub org/user name.
+  projectName: "ecole-doc", // Usually your repo name.
+  deploymentBranch: "gh-pages",
+  trailingSlash: false,
 
   onBrokenLinks: "throw",
   onBrokenMarkdownLinks: "warn",
